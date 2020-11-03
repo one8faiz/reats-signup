e4ec0cbbc8b15952522478d4f3cc657937021c50
one8faiz <73854066+one8faiz@users.noreply.github.com>
2020-11-03 04:44:16 -0800

    Add files via upload

---
+++ src/Form.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import "./Form.css";
+import FormSignup from "./FormSignup";
+import FormSuccess from "./FormSuccess";
+
+const Form = () => {
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  function submitForm() {
+    setIsSubmitted(true);
+  }
+  return (
+    <>
+      <div className="form-container">
+        <span className="close-btn">×</span>
+        <div className="form-content-left">
+          <h1> REGISTRATION </h1>
+          <h2>
+            {" "}
+            Reats is the new way of investing in real estate. Reats allows user
+            to invest in property at their own budget by buying shares
+          </h2>
+          <img className="form-img" src="img/img2.png" alt="spaceship" />
+        </div>
+        {!isSubmitted ? (
+          <FormSignup submitForm={submitForm} />
+        ) : (
+          <FormSuccess />
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Form;
